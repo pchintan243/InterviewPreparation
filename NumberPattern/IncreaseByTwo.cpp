74fc6051e463b15fd0e252be3460204618b7863c
chintan <patelchintan843@gmail.com>
2023-07-03 17:33:53 +0530

    increase by two number

---
+++ NumberPattern/IncreaseByTwo.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 5;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int k = i; k <= n - 1; k++)
+        {
+            cout << "  ";
+        }
+        for (int j = 1; j <= 2 * i - 1; j++)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
